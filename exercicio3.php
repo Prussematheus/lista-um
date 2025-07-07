7e76008a84715c8f7d66bf7c7970ecdf57800683
prussematheus <matheus_f_prusse@estudante.sesisenai.org.br>
2025-07-07 12:03:41 -0300

    Update - Exercício 3 resolvido

--- exercicio3.php
+++ exercicio3.php
@@ -2,13 +2,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Informa Primo</title>
+    <title>Informa Zero</title>
 </head>
 <body>
 
    <form method="POST" action="">
-        <label for="numero_primo">Verifica se é um número primo:</label>
-        <input type="number" id="numero" name="numero" required>
+        <label for="numero">Verifica se é menor, igual ou maior que zero:</label>
+        <input type="number" id="verificar_numero" name="numero" required>
         <button type="submit" name="verificar_zero">Verificar</button>
    </form>
 
@@ -16,23 +16,18 @@
    
    if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
-    if(isset($_POST['verificar_numero'])){
+    if(isset($_POST['verificar_zero'])){
         $numero = $_POST['numero'];
-        $ehZero = false;
-        if($numero < 0){
-            $ehZero = false;
-        }else{
-            for($i = 2; $i <= sqrt($numero); $i++){
-                if( $numero % $i == 0 ){
-                    $ehPrimo = false;
-                    break;
-                }
+        if($numero > 0){
+            echo "O número $numero é positivo";
+        }else if($numero == 0){
+            echo "O número $numero é zero";
+        }
+            else{
+                echo "O número $numero é negativo";
             }
         }
-        echo "O número $numero é ". ($ehPrimo ? 'primo':'não é primo');
-    };
-
-   };
+    }
 
    ?>
  
