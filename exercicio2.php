997806e80e2bacad436f29bdd76c54a9e1e79530
prussematheus <matheus_f_prusse@estudante.sesisenai.org.br>
2025-07-07 10:11:34 -0300

    Update - Exercicio 2 em progresso

--- exercicio2.php
+++ exercicio2.php
@@ -1,3 +1,34 @@
-<?php
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabuada</title>
+</head>
+<body>
 
-?>
+   <form method="POST" action="">
+        <label for="numero">Verifica a tabuada de número x:</label>
+        <input type="number" id="tabuada_numero" name="tabuada_numero" required>
+        <button type="submit" name="tabuada_numero">Verificar</button>
+   </form>
+
+   <?php
+   
+   if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    if(isset($_POST['tabuada_numero'])){
+        $numero = $_POST['numero'];
+        $ehPrimo = true;
+        $i = 1;
+       while($numero<=$numero*10);
+       echo $numero;
+       $numero*$i++;
+
+
+
+    };
+   };
+   ?>
+ 
+</body>
+</html>
