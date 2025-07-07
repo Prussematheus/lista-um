9ce3405f8368356471a220f426792b569ef98f6d
prussematheus <matheus_f_prusse@estudante.sesisenai.org.br>
2025-07-07 09:35:56 -0300

    Update - Exercício 1 resolvido

--- exercicio1.php
+++ exercicio1.php
@@ -1,3 +1,34 @@
-<?php
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Informa Par</title>
+</head>
+<body>
 
-?>
+   <form method="POST" action="">
+        <label for="numero">Verifica se é um número par ou ímpar:</label>
+        <input type="number" id="numero" name="numero" required>
+        <button type="submit" name="verificar_par">Verificar</button>
+   </form>
+
+   <?php
+   
+   if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    if(isset($_POST['verificar_par'])){
+        $numero = $_POST['numero'];
+        $ehPar = true;
+                if( $numero % 2 == 0 ){
+                    $ehPar = true;
+                }else{
+                    $ehPar = false;
+                };
+            };
+        };
+        echo "O número $numero é ". ($ehPar ? 'par':'ímpar');
+
+   ?>
+ 
+</body>
+</html>
