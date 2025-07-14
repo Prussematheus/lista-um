3ad2397d11a3814f79846ef614bc0eca6097deb0
prussematheus <matheus_f_prusse@estudante.sesisenai.org.br>
2025-07-14 08:37:34 -0300

    Update - Exercício 10 Resolvido

---
+++ exercicio10.php
@@ -0,0 +1,35 @@
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Informa Par</title>
+</head>
+<body>
+
+   <form method="POST" action="">
+        <label for="numero">Verifica se é um número par ou ímpar:</label>
+        <input type="number" id="numero" name="numero" required>
+        <button type="submit" name="verificar_par">Verificar</button>
+   </form>
+
+   <?php
+   
+   if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    if(isset($_POST['verificar_par'])){
+       $x = 0;
+       $y = 1;
+       $z = 0;
+       while ($b < $numero){
+        echo $b . '<br / > ';
+        $z = $x;
+        $x = $y;
+        $y = $y + $z;
+       }
+            };
+        };
+
+   ?>
+ 
+</body>
+</html>
